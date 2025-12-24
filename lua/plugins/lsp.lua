f68cf8ceaec7a60246321598b3ae57e23a3ed748
Pratham Khanal <prathamkhanal962@gmail.com>
2025-12-24 09:55:42 +0545

    adding tailwind and react

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,26 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls",
-                            "pyright",
-                            "html",
-                            "cssls" },
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {
-            ensure_installed = {
-                "stylua",
-                "prettier",
-                "black"}
-        } },
-        "neovim/nvim-lspconfig",
-    },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"lua_ls",
+			"pyright",
+			"ts_ls",
+			"html",
+			"cssls",
+			"tailwindcss",
+		},
+	},
+	dependencies = {
+		{
+			"mason-org/mason.nvim",
+			opts = {
+				ensure_installed = {
+					"stylua",
+					"prettier",
+					"black",
+				},
+			},
+		},
+		"neovim/nvim-lspconfig",
+	},
 }
